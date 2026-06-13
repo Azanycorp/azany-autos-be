dc6630c17efae2e75ba70c844e951f180d0e8051
onyiimesi <onyedikachukwu62@gmail.com>
2026-06-13 08:20:07 +0100

    Update

---
+++ routes/api/v1.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ForgotPasswordController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+        Route::controller(AuthenticationController::class)
+            ->group(function () {
+                Route::post('/register', 'register');
+                Route::post('/login', 'login');
+                Route::post('/verify-otp', 'verifyOtp');
+                Route::post('/verify-2fa', 'verify2fa');
+                Route::post('/resendVerificationMail', 'resendVerificationEmail');
+            });
+
+        Route::controller(ForgotPasswordController::class)
+            ->group(function () {
+                Route::post('/verify-user', 'verifyUser');
+                Route::post('/verify-code', 'verifyCode');
+                Route::post('/reset-password', 'changePassword');
+                Route::post('/resend-code', 'resendCode');
+            });
+    });
